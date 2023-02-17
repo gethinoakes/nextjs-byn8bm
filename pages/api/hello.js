b6f963adb43361316102a050680e39fc342e682f
Gethin Oakes <github@gethin.dev>
2023-02-17 22:41:43 +0000

    ggygyyt

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import axios from 'axios';
-const cheerio = require("cheerio");
+//const cheerio = require("cheerio");
 
 // import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -9,10 +9,11 @@ export default async (res) => {
   const url = 'https://en.wikipedia.org/wiki/Your_Article_Title_Here';
   try {
     const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-    const articleTitle = $('h1#firstHeading').text();
-    const articleContent = $('div#content').html();
-    res.status(200).json({ title: articleTitle, content: articleContent });
+     res.status(200).json(response.data);
+    //const $ = cheerio.load(response.data);
+    //const articleTitle = $('h1#firstHeading').text();
+    //const articleContent = $('div#content').html();
+    //res.status(200).json({ title: articleTitle, content: articleContent });
 } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
